033b2f2d746eff39ba45d992fbac413e5ce88622
Andrea Manzini <ilmanzo@gmail.com>
2023-01-30 12:55:41 +0100

    update todo

--- TODO.md
+++ TODO.md
@@ -1,17 +1,21 @@
 ## Todo
 
 - [ ] add more tests
+- [ ] implement more instructions
 - [ ] implement Containerfile, Dockerfile parsing
 - [ ] tidy: remove duplicates, merge "RUN" and "CMD" lines
 - [ ] add build feature that permits to run our image
 
 ## In progress
 
-- [ ] add github CI to run test suite on push
+- [ ] design parsing
 
 ## Done ✓
 
 - [x] Create and publish project
+- [x] add github CI to run test suite on push
+  - tests runs on linux, mac, windows
+
 
 
 
--- src/containertools.nim
+++ src/containertools.nim
@@ -1,58 +1,57 @@
 import macros except body
-from std/strutils import join,format,split
+from std/strutils import join, format, split
 from sequtils import mapIt
 
-const DEFAULT_BASE_IMAGE ="opensuse/leap";
-
-type 
-  ContainerInstruction = enum 
-    FROM, RUN, COPY, ENV, WORKDIR, EXPOSE, CMD , COMMENT , ADD, USER, VOLUME, LABEL, ENTRYPOINT, ARG, ONBUILD, STOPSIGNAL, HEALTHCHECK , SHELL 
-  ArgKind=enum uint16,string,openArray
-  ArgValue=object 
-      case kind: ArgKind:
-      of uint16:
-        ivalue : int
-      of string:
-        svalue : string
-      of openArray:
-        avalue : seq[string]
-  InstructionTuple=tuple[c: ContainerInstruction, v : ArgValue]        
+type
+  ContainerInstruction = enum
+    FROM, RUN, COPY, ENV, WORKDIR, EXPOSE, CMD, COMMENT, ADD, USER, VOLUME,
+        LABEL, ENTRYPOINT, ARG, ONBUILD, STOPSIGNAL, HEALTHCHECK, SHELL
+  ArgKind = enum uint16, string, openArray
+  ArgValue = object
+    case kind: ArgKind:
+    of uint16:
+      ivalue: int
+    of string:
+      svalue: string
+    of openArray:
+      avalue: seq[string]
+  InstructionTuple = tuple[c: ContainerInstruction, v: ArgValue]
 
   Spec = object
-    instructions:seq[InstructionTuple]
-    entrypoint: string 
-    baseImage : string 
+    instructions: seq[InstructionTuple]
+    entrypoint: string
+    baseImage: string
 
-proc parseSpec*(spec: string) : Spec =
+proc parseSpec*(spec: string): Spec =
   discard
 
 # returns string representation of a container image
 proc `$`*(self: Spec): string =
-  var tmpout : seq[string]
+  var tmpout: seq[string]
   for item in self.instructions:
-    let value=item.v
-    let outvalue = case value.kind 
+    let value = item.v
+    let outvalue = case value.kind
       of uint16: $value.ivalue
       of string: value.svalue
       of openArray: "[" & value.avalue.mapit('"' & $it & '\"').join(",") & "]"
-    case item.c: 
-      of COMMENT: 
+    case item.c:
+      of COMMENT:
         tmpout.add("# $1".format(outvalue))
       else:
-        tmpout.add("$1 $2".format(item.c,outvalue))
+        tmpout.add("$1 $2".format(item.c, outvalue))
   tmpout.join("\n")
-  
+
 
 # instead of writing a bunch of templates, like
-# 
+#
 # template FROM*(baseImage: string) = s.add(FROM, baseImage)
 # template COMMENT*(text: string) = s.add(COMMENT, text)
 #
 # we simply generate with a macro
-macro autoGenAllTheTemplates() = 
-  var allTemplates : string
+macro autoGenAllTheTemplates() =
+  var allTemplates: string
   for item in ContainerInstruction:
-    if item==EXPOSE or item==CMD:
+    if item == EXPOSE or item == CMD:
       continue
     let cmd = $item
     allTemplates.add "template $1*(value: string) = spec.instructions.add ($1,ArgValue(kind: string, svalue: value))\n".format(cmd)
@@ -60,14 +59,14 @@ macro autoGenAllTheTemplates() =
 
 autoGenAllTheTemplates()
 # some commands have special cases for argument type
-template EXPOSE*(port: uint16) = spec.instructions.add (EXPOSE,ArgValue(kind: uint16, ivalue: port))
-template CMD*(items: openArray) = spec.instructions.add (CMD,ArgValue(kind: openArray, avalue: items))
-template CMD*(arg: string) = spec.instructions.add (CMD,ArgValue(kind: openArray, avalue: arg.split))
+template EXPOSE*(port: uint16) =
+  spec.instructions.add (EXPOSE, ArgValue(kind: uint16, ivalue: port))
+template CMD*(items: openArray) =
+  spec.instructions.add (CMD, ArgValue(kind: openArray, avalue: items))
+template CMD*(arg: string) =
+  spec.instructions.add (CMD, ArgValue(kind: openArray, avalue: arg.split))
 
-template containerSpec*(cmds: untyped) : string =
-  var spec {.inject.} : Spec
-  cmds
+template containerSpec*(instructions: untyped): string =
+  var spec {.inject.}: Spec
+  instructions
   $spec
-
-
-
